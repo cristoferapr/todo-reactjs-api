a04d279584a1c280c5e25e880ab227948cdea951
cristofer-apr <cpr.refor@gmail.com>
2023-03-29 23:45:01 +0000

    implementacion inicial

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	const [input, setInput] = useState('');
+	const [task, setTask] = useState([])
+	const [toggle, setToogle] = useState("No tasks, add a task")
+	
+	const togglePh = (n) =>{
+		if (n == 1){
+			return "What's need to be done?"
+		}else{
+			return "No tasks, add a task"
+		}
+	}
 
-export default Home;
+	const addTask = (e) =>{
+		e.preventDefault()
+		if (input === '') return
+		setTask([...task, {
+			text:input,
+			completed:false
+		}])
+		setInput('')
+		if( toggle === "No tasks, add a task"){
+			setToogle(togglePh(1))
+		}
+		console.log(toggle)
+	}
+
+	const remove = (index) =>{
+		setTask(task.filter((_item,i)=> i !== index))
+		if (index == 0){
+			togglePh(0)
+		}
+		if( task.length === 1){
+			setToogle(togglePh(0))
+		}
+	}
+
+	return(
+		<body>
+	<div className="container-fluid text-center body">
+		<h1 className="heading">todos</h1>
+		<ul>
+			<li>
+				<form onSubmit={addTask}>
+				<input placeholder={toggle} className="taskinput ps-1" onChange={e => setInput(e.target.value)} value={input}></input>
+				<button className="add-btn"></button>
+				</form>
+			</li>
+			{task.map((item, i) =>(
+				<li key={i}>
+					<button className="delete-btn" onClick={()=> remove(i)}>{item.text}<span>x</span></button>
+				</li>
+			))}
+		</ul>
+	</div>
+	</body>
+	)
+}
+  
+  export default Home
