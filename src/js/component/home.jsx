6dd434123f618579854ca6c72ea7a658307ae818
cristofer-apr <cpr.refor@gmail.com>
2023-03-31 05:19:08 +0000

    added api functions

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,10 @@
-import React, {useState} from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState, useEffect} from "react";
 
 //create your first component
 const Home = () => {
 	const [input, setInput] = useState('');
 	const [task, setTask] = useState([])
 	const [toggle, setToogle] = useState("No tasks, add a task")
-	
 	const togglePh = (n) =>{
 		if (n == 1){
 			return "What's need to be done?"
@@ -17,13 +13,102 @@ const Home = () => {
 		}
 	}
 
+	async function fetchGET (){
+		const response = await fetch('https://assets.breatheco.de/apis/fake/todos/user/cristofer', {
+		  method: "GET",
+		})
+		const responseJSON = await response.json()
+		if (!responseJSON) fetchCREATE()
+		setTask(responseJSON)
+		if( responseJSON.length > 1) setToogle(togglePh(1))
+
+		.then(resp => {
+	
+			console.log(resp.ok); // will be true if the response is successfull
+
+			console.log(resp.status); // the status code = 200 or code = 400 etc.
+	
+			console.log(resp.text()); // will try return the exact result as string
+	
+			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+	
+		})
+	
+		.then(data => {
+	
+			//here is were your code should start after the fetch finishes
+	
+			console.log(data); //this will print on the console the exact object received from the server
+	
+		})
+	
+		.catch(error => {
+	
+			//error handling
+	
+			console.log(error);
+	
+		});
+	}
+
+	async function fetchPUT (){
+		console.log(task)
+		await fetch('https://assets.breatheco.de/apis/fake/todos/user/cristofer', {
+
+		method: "PUT",
+  
+		body: JSON.stringify(task),
+  
+		headers: {
+  
+		  "Content-Type": "application/json"
+  
+		}
+  
+	  })
+
+		.then(resp => {
+	
+			console.log(resp.ok); // will be true if the response is successfull
+	
+			console.log(resp.status); // the status code = 200 or code = 400 etc.
+	
+			console.log(resp.text()); // will try return the exact result as string
+	
+			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+	
+		})
+	
+		.then(data => {
+	
+			//here is were your code should start after the fetch finishes
+	
+			console.log(data); //this will print on the console the exact object received from the server
+	
+		})
+	
+		.catch(error => {
+	
+			//error handling
+	
+			console.log(error);
+	
+		});
+	}
+
+	useEffect (() =>{
+		fetchGET()
+	}, [])
+
+	useEffect (() =>{
+		fetchPUT()
+	}, [task])
+
 	const addTask = (e) =>{
 		e.preventDefault()
 		if (input === '') return
-		setTask([...task, {
-			text:input,
-			completed:false
-		}])
+		setTask([...task,
+			{label:input, done:false}])
 		setInput('')
 		if( toggle === "No tasks, add a task"){
 			setToogle(togglePh(1))
@@ -33,12 +118,19 @@ const Home = () => {
 
 	const remove = (index) =>{
 		setTask(task.filter((_item,i)=> i !== index))
-		if (index == 0){
-			togglePh(0)
-		}
-		if( task.length === 1){
+		if( task.length === 2){
 			setToogle(togglePh(0))
 		}
+	}
+
+	const removeAll = () =>{
+		var j = task.length -1
+		let reducedTodo = [...task]
+		for (j ; j>0; j--){
+			reducedTodo.splice (j)
+		}
+		setTask(reducedTodo)
+		setToogle(togglePh(0))
 	}
 
 	return(
@@ -54,10 +146,13 @@ const Home = () => {
 			</li>
 			{task.map((item, i) =>(
 				<li key={i}>
-					<button className="delete-btn" onClick={()=> remove(i)}>{item.text}<span>x</span></button>
+					<button className="delete-btn" onClick={()=> remove(i)}>{item.label}<span>x</span></button>
 				</li>
 			))}
+					<br></br>
+		<button className="rmall" onClick={removeAll}>REMOVE ALL TASKS</button>
 		</ul>
+
 	</div>
 	</body>
 	)
